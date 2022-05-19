1566799694eb303393a72b460b7938a890707a8e
Edem <edem.adjovi@efrei.net>
2022-05-20 00:10:43 +0200

    creation de dev_lot2

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,8 +1,29 @@
-//
-// Created by edema on 18/05/2022.
-//
+#ifndef DOUBLY_LINKED_LIST
+#define DOUBLY_LINKED_LIST
 
-#ifndef MVS_HUGENUMBERCALCULATOR_DOUBLYLINKEDLIST_H
-#define MVS_HUGENUMBERCALCULATOR_DOUBLYLINKEDLIST_H
+typedef unsigned short Digit;
 
-#endif //MVS_HUGENUMBERCALCULATOR_DOUBLYLINKEDLIST_H
+typedef struct Node {
+    Digit digit;
+    struct Node* next;
+    struct Node* previous;
+} Node;
+
+typedef struct DoublyLinkedList {
+    Node* start;
+    Node* end;
+} DoublyLinkedList;
+
+extern int               addDigitAtStart                  (DoublyLinkedList* doublyLinkedList, const Digit digit);
+extern int               addDigitAtEnd                    (DoublyLinkedList* doublyLinkedList, const Digit digit);
+extern int               isNodeEmpty                      (const Node* node);
+extern DoublyLinkedList* createDoublyLinkedList           (void);
+extern DoublyLinkedList* createDoublyLinkedListFromString (const char* digitString);
+extern char              digitToChar                      (const Digit digit);
+extern void              deleteDoublyLinkedList           (DoublyLinkedList* doublyLinkedList);
+extern int               getDoublyLinkedListLength        (const DoublyLinkedList* doublyLinkedList);
+extern void              removeStartFromDoublyLinkedList  (DoublyLinkedList* doublyLinkedList);
+extern void              removeEndFromDoublyLinkedList    (DoublyLinkedList* doublyLinkedList);
+extern Digit             getDigitByPlaceFromStart         (const DoublyLinkedList* doublyLinkedList, const unsigned int place);
+
+#endif // !DOUBLY_LINKED_LIST
